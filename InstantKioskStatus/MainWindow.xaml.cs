cc0584a61ad8f1d9a043529934e5362831bf311e
diyarfaraj <diyar.faraj@gmail.com>
2021-02-07 13:14:43 +0100

    added kiosk status icons

--- InstantKioskStatus/MainWindow.xaml.cs
+++ InstantKioskStatus/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -24,19 +26,104 @@ namespace InstantKioskStatus
         {
             InitializeComponent();
 
+            //CanvasMain.Background = 
+            
             ImageBrush myBrush = new ImageBrush();
             myBrush.ImageSource =
-                new BitmapImage(new Uri("https://instantsystems.se/wp-content/uploads/2019/10/padel-kiosk-2-600x1142.png"));
+                new BitmapImage(new Uri("C://Users/Diyar/Downloads/instant-cooler-600-5-800x1726-removebg.png"));
+
+            ImageBrush check = new ImageBrush();
+            check.ImageSource =
+                new BitmapImage(new Uri("C://Users/Diyar/Downloads/good_or_tick.png"));
+
+            ImageBrush fail = new ImageBrush();
+            fail.ImageSource =
+                new BitmapImage(new Uri("C://Users/Diyar/Downloads/alert.png"));
+
+            ImageBrush neutral = new ImageBrush();
+            neutral.ImageSource =
+                new BitmapImage(new Uri("C://Users/Diyar/Downloads/questionmark.png"));
+
+
 
             var posValue = 10;
-            Ellipse[] kiosks = new Ellipse[5];
+            Grid[] kiosks = new Grid[5];
             for (int i = 0; i < kiosks.Length; i++)
             {
-                posValue += 50; 
-                kiosks[i] = new Ellipse() { Width = 100, Height = 100, Fill = myBrush };
+                kiosks[i] = new Grid();
+
+                var rectangle = new Rectangle()
+                {
+
+                    //Stroke = Brushes.Black,
+                    Fill = myBrush,
+                    Width = 100,
+                    Height = 200,
+                    Margin = new Thickness(
+                    left: 0,
+                    top: 20,
+                    right: 0,
+                    bottom: 0),
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    VerticalAlignment = VerticalAlignment.Top
+
+                };
+                posValue += 50;
+                kiosks[i].Children.Add(rectangle);
+                //kiosks[i] = new Grid() { Width = 100, Height = 100};
+                
                 Canvas.SetTop(kiosks[i], posValue);
                 Canvas.SetLeft(kiosks[i], 20);
                 kiosks[i].PreviewMouseDown += UserCTRL_PreviewMouseDown;
+                TextBlock textblock = new TextBlock();
+                textblock.Text = "Kiosk ID: " + (i + 1).ToString();
+                
+
+                textblock.HorizontalAlignment = HorizontalAlignment.Center;
+                Ellipse approvedIcon = new Ellipse() { Height = 40, Width = 40, Fill = check };
+                Ellipse failIcon = new Ellipse() { Height = 40, Width = 40, Fill = fail };
+                Ellipse neutralIcon = new Ellipse() { Height = 40, Width = 40, Fill = neutral };
+
+
+                approvedIcon.VerticalAlignment = VerticalAlignment.Center;
+                approvedIcon.HorizontalAlignment = HorizontalAlignment.Center;
+                failIcon.VerticalAlignment = VerticalAlignment.Center;
+                failIcon.HorizontalAlignment = HorizontalAlignment.Center;
+
+                ScaleTransform trans = new ScaleTransform();
+                failIcon.RenderTransform = trans;
+                DoubleAnimation anim = new DoubleAnimation(1, 1.2, TimeSpan.FromMilliseconds(5000));
+                anim.RepeatBehavior = RepeatBehavior.Forever;
+                BounceEase b = new BounceEase();
+                b.Bounces = 5;
+                b.Bounciness = 1;
+                b.EasingMode = EasingMode.EaseIn;
+
+                anim.EasingFunction = b;
+                trans.BeginAnimation(ScaleTransform.ScaleXProperty, anim);
+                trans.BeginAnimation(ScaleTransform.ScaleYProperty, anim);
+
+
+                kiosks[i].Children.Add(textblock);
+                kiosks[i].Children.Add(approvedIcon);
+
+                if (i == 1)
+                {
+                    kiosks[i].Children.Remove(approvedIcon);
+
+                    kiosks[i].Children.Add(neutralIcon);
+                }
+
+                if (i == kiosks.Length - 1)
+                {
+                    kiosks[i].Children.Remove(approvedIcon);
+
+                    kiosks[i].Children.Add(failIcon);
+
+                }
+
+
+
                 CanvasMain.Children.Add(kiosks[i]);
             }
 
